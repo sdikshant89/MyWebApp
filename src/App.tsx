596ebb8cc1795b28e648e88937aa4d8e191389e8
Dikshant Sharma <sdikshant89@gmail.com>
2024-02-10 20:27:56 -0700

    [Feb 10 08.30pm] Pushing modifications to basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,13 +9,10 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,9 +21,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
---
+++ src/assets/learnings.tsx
@@ -0,0 +1,9 @@
+/**
+
+    TIPS:
+    For react use tsx
+    
+    SHORTCUTS:
+    to change multiple occurance of the same symbol, select -> *command + D*
+
+ */
