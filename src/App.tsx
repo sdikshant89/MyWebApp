4bc59abca9c1205f8fcd0d58e3ec28eb8b120bfb
Dikshant Sharma <sdikshant89@gmail.com>
2024-02-20 14:22:52 -0700

    [Feb 20 02.20pm] Added material ui and tried using drawer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 import viteLogo from './assets/vite.svg'
+import Drawer from '@mui/material/Drawer'
+import Paper from '@mui/material/Paper'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [openDrawer, setopenDrawer] = useState(false)
+
+  const toggleDrawer = ()=>{
+    setopenDrawer((prevState) => !prevState);
+  }
 
   return (
     <div className="App">
@@ -21,6 +28,21 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <>
+        <button onClick={() => toggleDrawer()}>
+          {openDrawer? 'Close Drawer': 'Open Drawer'}
+        </button>
+        <Drawer
+            anchor='right'
+            open={openDrawer}
+            onClose={toggleDrawer}
+            PaperProps={{style: {backgroundColor: 'yellow'}}}
+          >
+            <p>
+          Edit  HMR
+        </p>
+          </Drawer>
+      </>
     </div>
   )
 }
